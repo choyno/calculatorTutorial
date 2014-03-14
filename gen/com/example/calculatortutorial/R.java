451cd3570d4bf2ba835cbb5922014cd4af3b5d81
choyno <ariel.lopezf@gmail.com>
2014-03-15 01:38:18 +0800

    operator

--- gen/com/example/calculatortutorial/R.java
+++ gen/com/example/calculatortutorial/R.java
@@ -24,25 +24,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int btn_0=0x7f08000a;
-        public static final int btn_1=0x7f080009;
-        public static final int btn_2=0x7f080008;
-        public static final int btn_3=0x7f080007;
-        public static final int btn_4=0x7f080004;
-        public static final int btn_5=0x7f080005;
-        public static final int btn_6=0x7f080006;
+        public static final int action_settings=0x7f080013;
+        public static final int btn_0=0x7f08000b;
+        public static final int btn_1=0x7f08000a;
+        public static final int btn_2=0x7f080009;
+        public static final int btn_3=0x7f080008;
+        public static final int btn_4=0x7f080005;
+        public static final int btn_5=0x7f080006;
+        public static final int btn_6=0x7f080007;
         public static final int btn_7=0x7f080001;
-        public static final int btn_8=0x7f080002;
-        public static final int btn_9=0x7f080003;
-        public static final int btn_add=0x7f08000d;
-        public static final int btn_c=0x7f080011;
-        public static final int btn_dot=0x7f08000b;
-        public static final int btn_equals=0x7f08000c;
-        public static final int btn_minus=0x7f08000e;
-        public static final int btn_mul=0x7f080010;
-        public static final int btn_times=0x7f08000f;
-        public static final int editText1=0x7f080000;
+        public static final int btn_8=0x7f080003;
+        public static final int btn_9=0x7f080004;
+        public static final int btn_add=0x7f08000e;
+        public static final int btn_c=0x7f080012;
+        public static final int btn_dot=0x7f08000c;
+        public static final int btn_equals=0x7f08000d;
+        public static final int btn_minus=0x7f08000f;
+        public static final int btn_mul=0x7f080011;
+        public static final int btn_times=0x7f080010;
+        public static final int editText1=0x7f080002;
+        public static final int txt_input_holder=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/calculatortutorial/MainActivity.java
+++ src/com/example/calculatortutorial/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.calculatortutorial;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
+	private EditText input_holder; 
+	private String operator;
+	private float number_bf;
+	private ButtonOnClickListener btnOnClick;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		input_holder = (EditText) findViewById(R.id.txt_input_holder);
+		input_holder.setText("0");
+		int btnList[] = {R.id.btn_0, R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4, R.id.btn_5, R.id.btn_6
+				, R.id.btn_7, R.id.btn_8, R.id.btn_9, R.id.btn_dot, R.id.btn_c, R.id.btn_add, R.id.btn_equals
+				, R.id.btn_minus, R.id.btn_times, R.id.btn_mul};
+		
+		for(int id:btnList){
+			View v =(View) findViewById(id);
+			v.setOnClickListener(btnOnClick);
+		}
+		
 	}
 
 	@Override
@@ -17,6 +37,64 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void mExecute(String str){	 
+		number_bf = Float.parseFloat(input_holder.getText().toString());
+		operator = str;
+		input_holder.setText("0");
+	} 
+	public void mResult(){ 
+		float number_af = Float.parseFloat(input_holder.getText().toString());
+		float result = 0;
+		if(operator == "+"){
+			result = number_bf + number_af; 
+		}else if(operator == "-"){
+			result = number_bf - number_af; 
+		}else if(operator == "*"){
+			result = number_bf * number_af; 
+		}else if(operator == "/"){
+			result = number_bf / number_af; 
+		}
+		
+		input_holder.setText(String.valueOf(result));
+	}  
+	public void getKeyClick(String str){
+		String addValue = input_holder.getText().toString();
+		addValue += str;
+		input_holder.setText("0");
+	}
+	
+	private class ButtonOnClickListener implements OnClickListener{
+		
+		public void onClick(View view){ 
+			switch(view.getId()){
+			case(R.id.btn_c):
+				input_holder.setText("0");
+				operator = "";
+				number_bf = 0;
+				break; 
+			case(R.id.btn_add): 
+				mExecute("+");
+				break;  
+			case(R.id.btn_minus): 
+				mExecute("-");
+				break;  
+			case(R.id.btn_times): 
+				mExecute("*");
+				break;  
+			case(R.id.btn_mul): 
+				mExecute("/");
+				break;  
+			case(R.id.btn_equals):
+				mResult();
+				break;
+			default:
+				String btn_value = ((Button) view).getText().toString();
+				getKeyClick(btn_value);
+				break;
+			}
+		} 
 	}
 
 }
